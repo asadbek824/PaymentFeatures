0eef4068651fea4e647d2e3626fe81acc0021433
akbarshah19 <akbarshah19@icloud.com>
2025-04-07 17:46:21 +0500

    Custom Receipt Alert init

---
+++ Features/PaymentApp/Payment/ReceiptView/ReceiptView.swift
@@ -0,0 +1,66 @@
+//
+//  ReceiptView.swift
+//  Payment
+//
+//  Created by Akbarshah Jumanazarov on 4/7/25.
+//
+
+import Core
+import SwiftUI
+
+enum ReceiptType {
+    case sent, received
+}
+
+struct ReceiptView: View {
+    
+    var type: ReceiptType
+    var peerMessage: PeerMessage
+    @Binding var show: Bool
+    
+    var body: some View {
+        VStack {
+            VStack {
+                Image(systemName: "checkmark")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 50, height: 50)
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(.green.gradient)
+                    .clipShape(.circle)
+                    .transition(.opacity.combined(with: .scale))
+                
+                Text(type == .sent ? "Sent" : "Received")
+                    .font(.system(size: 35, weight: .bold))
+                
+                Button {
+                    show.toggle()
+                } label: {
+                    Text("OK")
+                        .padding()
+                        .fontWeight(.medium)
+                        .foregroundStyle(.white)
+                        .frame(maxWidth: .infinity)
+                        .background(.appPrimary)
+                        .clipShape(.rect(cornerRadius: 8))
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(.yellow)
+            .clipShape(.rect(cornerRadius: 16))
+        }
+        .padding()
+        .fillSuperview()
+        .background(.systemBackground.opacity(0.1))
+        .animation(.smooth, value: show)
+        .onTapGesture {
+            show.toggle()
+        }
+    }
+}
+
+//#Preview {
+//    ReceiptView(type: .sent, show: .constant(true))
+//}
