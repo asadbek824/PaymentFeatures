c6d78996b00ced5300e16916e0e2717e97c5fbce
asadbek824 <yoldoshev5007@icloud.com>
2025-04-13 18:08:29 +0500

    [develop] lock screen widget completed

--- PaymeShareWidget/PaymeLockScreenWidget.swift
+++ PaymeShareWidget/PaymeLockScreenWidget.swift
@@ -42,10 +42,15 @@ struct LockScreen_WidgetEntryView: View {
 
     var body: some View {
         Link(destination: URL(string: "paymeFeature2://pay-share")!) {
-            Image(.payShareLockScreen)
-                .resizable()
-                .scaledToFit()
-                .clipShape(.circle)
+            ZStack {
+                Circle()
+                    .strokeBorder(Color.white.opacity(0.3), lineWidth: 2)
+                    .background(Circle().fill(Color.white.opacity(0.15)))
+                Image(.payShareLogo)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(8)
+            }
         }
     }
 }
--- PaymeShareWidget/Widgets/PaymeLockScreenWidget.swift
+++ PaymeShareWidget/Widgets/PaymeLockScreenWidget.swift
@@ -8,22 +8,22 @@
 import WidgetKit
 import SwiftUI
 
-struct PaymeLockScreenWidget: Widget {
-    let kind: String = "PaymeLockScreenWidget"
-
-    var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: LockScreenProvider()) { entry in
-            if #available(iOS 17.0, *) {
-                LockScreenWidgetView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
-            } else {
-                LockScreenWidgetView(entry: entry)
-                    .padding()
-                    .background(.ultraThinMaterial)
-            }
-        }
-        .configurationDisplayName("PayShare Lock")
-        .description("Быстрый доступ к PayShare с экрана блокировки")
-        .supportedFamilies([.accessoryCircular])
-    }
-}
+//struct PaymeLockScreenWidget: Widget {
+//    let kind: String = "PaymeLockScreenWidget"
+//
+//    var body: some WidgetConfiguration {
+//        StaticConfiguration(kind: kind, provider: LockScreenProvider()) { entry in
+//            if #available(iOS 17.0, *) {
+//                LockScreenWidgetView(entry: entry)
+//                    .containerBackground(.fill.tertiary, for: .widget)
+//            } else {
+//                LockScreenWidgetView(entry: entry)
+//                    .padding()
+//                    .background(.ultraThinMaterial)
+//            }
+//        }
+//        .configurationDisplayName("PayShare Lock")
+//        .description("Быстрый доступ к PayShare с экрана блокировки")
+//        .supportedFamilies([.accessoryCircular])
+//    }
+//}
